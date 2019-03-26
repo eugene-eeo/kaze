ad596aad0534a1914c2f6fdb94322d4981f842c8
Eeo Jun <141bytes@gmail.com>
2019-03-26 23:06:54 +0000

    rethink line.go

--- render/line.go
+++ render/line.go
@@ -1,29 +1,52 @@
 package render
 
+import "github.com/BurntSushi/xgbutil/xgraphics"
 import "github.com/BurntSushi/freetype-go/freetype/truetype"
 
 type Chunk struct {
-	Text string
+	W    int
+	H    int
+	Rune rune
 	Font *truetype.Font
 }
 
-func ChunksFromString(text string, fl FontList) []Chunk {
-	var prev *truetype.Font
-	chunks := []Chunk{}
-	t := ""
+type Line struct {
+	Chunks []Chunk
+	Height int
+	Width  int
+}
+
+type TextBox []Line
+
+func ChunksFromString(text string, fl FontList, fontSize float64, cb func(Chunk)) {
+	c := Chunk{}
 	for _, r := range text {
 		font := fl.FontSupporting(r)
-		if font == prev {
-			t += string(r)
+		c.W, c.H = xgraphics.Extents(font, fontSize, string(r))
+		c.Font = font
+		c.Rune = r
+		cb(c)
+	}
+}
+
+func TextBoxWithMaxWidth(text string, fl FontList, fontSize float64, maxWidth int) TextBox {
+	tb := TextBox{}
+	line := Line{Chunks: []Chunk{}}
+	ChunksFromString(text, fl, fontSize, func(c Chunk) {
+		if line.Width+c.W > maxWidth {
+			tb = append(tb, line)
+			line = Line{
+				Chunks: []Chunk{c},
+				Height: c.H,
+				Width:  c.W,
+			}
 		} else {
-			chunks = append(chunks, Chunk{t, font})
-			t = ""
-			prev = font
+			line.Chunks = append(line.Chunks, c)
+			line.Width += c.W
+			if line.Height < c.H {
+				line.Height = c.H
+			}
 		}
-	}
-	// In case everything is in the same font
-	if t != "" && prev != nil {
-		chunks = append(chunks, Chunk{t, prev})
-	}
-	return chunks
+	})
+	return tb
 }
